214aab1a40bd98ea0f8425fbb6a171fe5eba52b5
Carlos Siqueira <csiqueira@eb.com>
2023-04-17 08:19:31 -0300

    Add fonts and normilize

--- src/pages/Cardápio/index.tsx
+++ src/pages/Cardápio/index.tsx
@@ -1,5 +1,6 @@
 import Botao from './Botao';
 import styles from './Cardapio.module.scss';
+import 'normalize.css';
 import { ReactComponent as Logo } from 'assets/logo.svg'
 
 export default function Cardapio() {
@@ -7,6 +8,7 @@ export default function Cardapio() {
         <main>
             <nav className={styles.menu}>
                 <Logo/>
+                Aluroni
             </nav>
         </main>
     )
