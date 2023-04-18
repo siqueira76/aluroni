7fd06b39670aa054195babaf8d0073f73c7f7316
Carlos Siqueira <csiqueira@eb.com>
2023-04-18 20:56:07 -0300

    Header and Buscador

--- src/pages/Cardápio/index.tsx
+++ src/pages/Cardápio/index.tsx
@@ -1,9 +1,12 @@
-import Botao from './Botao';
 import styles from './Cardapio.module.scss';
 import 'normalize.css';
 import { ReactComponent as Logo } from 'assets/logo.svg'
+import Buscador from './buscador';
+import { useState } from 'react';
 
 export default function Cardapio() {
+    const [busca, setBusca] = useState("");
+
     return(
         <main>
             <nav className={styles.menu}>
@@ -15,6 +18,10 @@ export default function Cardapio() {
                 A casa do código e da massa
                 </div>
             </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardapio</h3>
+                <Buscador busca={busca} setBusca={setBusca}/>
+            </section>
         </main>
     )
 }
