13a19b3912429284a3a05bfa88df55774ea55a55
Carlos Siqueira <csiqueira@eb.com>
2023-05-01 10:13:51 -0300

    sort order

--- src/pages/Cardápio/itens/index.tsx
+++ src/pages/Cardápio/itens/index.tsx
@@ -12,7 +12,7 @@ interface Props {
 export default function Itens(props: Props){
     
     const [lista, setLista] = useState(cardapio);
-    const {busca, filtro} = props;
+    const {busca, filtro, ordenador} = props;
 
     function testaBusca(title: string){
         const regex = new RegExp(busca, 'i');
@@ -24,10 +24,24 @@ export default function Itens(props: Props){
         return true;
     }    
 
+    function ordenar(novaLista: typeof cardapio){
+        switch(ordenador){
+            case 'porcao':
+                return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+            case 'qtd_pessoas':
+                return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
+            case 'preco':
+                return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
+            default:
+                return novaLista;
+        }
+    }
+
+
     useEffect(() => {
         const novaLista = cardapio.filter(Item => testaBusca(Item.title) && testaFiltro(Item.category.id));   
-        setLista(novaLista);
-    }, [busca, filtro]);
+        setLista(ordenar(novaLista));
+    }, [busca, filtro, ordenador]);
 
     return(
         <div className={styles.itens}>
